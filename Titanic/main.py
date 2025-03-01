eb13c7b1692f5422bc2e38bc095ce9ceda94b33d
amylee <amy.js.lee94@gmail.com>
2025-03-01 16:11:24 -0600

    cleaning

--- Titanic/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-sys.path.insert(0, parent_dir)
-sys.path = list(dict.fromkeys(sys.path))
-print(parent_dir)
-
-# from modules.preproc import LogisticRegression as t
-# print(t.test())
